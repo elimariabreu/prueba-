cf9402ba767468e42f89952305dadde695c3d448
Estudiantes <Estudiantes@labing50417.udistrital.edu.co>
2017-08-11 16:24:18 -0500

    Hola Mundo

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -2,9 +2,12 @@ package prueba;
 
 public class Prueba {
 
-    
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
-    
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hola Mundo");
+        }
+
+   }
 }
+
